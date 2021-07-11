7f1344fe141dd6fae2dc4b46aa38f476415dcee6
Bernhard Pottler <bpottler@nexgo.de>
2021-07-11 06:59:37 +0200

    test: add arraySupport to srx/index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,11 @@
 import dayjs from 'dayjs/esm';
 import advancedFormat from 'dayjs/esm/plugin/advancedFormat';
 import utc from 'dayjs/esm/plugin/utc';
+import arraySupport from 'dayjs/esm/plugin/arraySupport';
 
 dayjs.extend(advancedFormat);
+dayjs.extend(utc);
+dayjs.extend(arraySupport);
 
 console.log('** Demo Dayjs using "import from \'dayjs/esm\'" **')
 
@@ -18,8 +21,8 @@ console.log(`dayjs(isostring): ${demoIso}`);
 const demoAdvancedFormat = dayjs().format('Q Do k kk X x');
 console.log(`advancedFormat: ${demoAdvancedFormat}`);
 
-// from here on:use utc
-dayjs.extend(utc);
-
 const formatUtc = dayjs.utc().format();
 console.log(`dayjs.utc().format(): ${formatUtc}`);
+
+const formatUtcFromArray = dayjs.utc([2010, 1, 14, 15, 25, 50, 125]).format();
+console.log(`dayjs.utc([...]).format(): ${formatUtcFromArray}`);
