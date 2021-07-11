1a5d9f954ea0577372e47af49f2be22ebc458030
Bernhard Pottler <bpottler@nexgo.de>
2021-07-11 11:21:48 +0200

    test: extend test for locale

--- test/dayjs.locale.spec.ts
+++ test/dayjs.locale.spec.ts
@@ -4,11 +4,29 @@
 
 import dayjs from 'dayjs/esm';
 import 'dayjs/esm/locale/de';
+import localizedFormat from 'dayjs/esm/plugin/localizedFormat';
+
+dayjs.extend(localizedFormat);
+
+// Set global locale
+dayjs.locale('de');
 
 describe('locale', () => {
-  it('should use locale to format date', () => {
-    const result = dayjs('2018-10-07').locale('de').format('L');
+  it('should get current global locale', () => {
+    const result = dayjs.locale();
+
+    expect(result).toEqual('de');
+  });
+
+  it('should use global locale to format date', () => {
+    const result = dayjs('2018-10-07').format('L');
 
     expect(result).toEqual('07.10.2018');
+  });
+
+  it('should use "en" locale to format date', () => {
+    const result = dayjs('2018-10-07').locale('en').format('L');
+
+    expect(result).toEqual('10/07/2018');
   });
 });
